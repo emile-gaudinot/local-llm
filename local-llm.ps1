b7e306e459447ea2686ca225d909921c34b510dd
emile-gaudinot <emile.gaudinot@gmail.com>
2025-11-11 22:28:58 +0100

    Update 2 with Ollama

---
+++ local-llm.ps1
@@ -0,0 +1,6 @@
+cd D:\Documents\local-llm\app
+start http://localhost:3000
+.\get_model_names.ps1
+node server.js
+
+Read-Host
--- mistral-local.ps1
+++
@@ -1,5 +0,0 @@
-cd D:\Documents\local-llm\mistral-ui
-start http://localhost:3000
-node server.js
-
-Read-Host
